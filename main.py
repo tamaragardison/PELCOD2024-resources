0a532f3987d3ccd046d2e872229a3028a9b38c15
tamaragardison <tamara45532@gmail.com>
2024-09-21 15:15:45 +0700

    latihan coding 21 sep 2024, programer handal soon

---
+++ main.py
@@ -0,0 +1,10 @@
+nilai = int(input("masukkan nilai:"))
+
+if (nilai > 80):
+     print("selamat anda lulus")
+elif(nilai > 90 and nilai <= 100):
+     print("selamat anda lulus, dengan nilai yang memuaskan")
+else:
+     print("anda masih belum lulus tes, semangat dan coba lagi")
+
+
